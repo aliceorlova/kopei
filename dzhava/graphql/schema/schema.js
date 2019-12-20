14b11d2be43feb0e2f9476cbb6ec6fb1e1ceb959
Alice <alice.orlovaa@gmail.com>
2019-12-20 17:05:17 +0200

    fixed names

--- dzhava/graphql/schema/schema.js
+++ dzhava/graphql/schema/schema.js
@@ -9,15 +9,9 @@ const {
     GraphQLList, GraphQLNonNull
 } = graphql;
 
-//Schema defines data on the Graph like object types(book type), relation between 
-//these object types and describes how it can reach into the graph to interact with 
-//the data to retrieve or mutate the data   
 
 const CarType = new GraphQLObjectType({
     name: 'Car',
-    //We are wrapping fields in the function as we dont want to execute this ultil 
-    //everything is inilized. For example below code will throw error AuthorType not 
-    //found if not wrapped in a function
     fields: () => ({
         id: { type: GraphQLID },
         model: { type: GraphQLString },
@@ -35,7 +29,7 @@ const UserType = new GraphQLObjectType({
         booking: {
             type: new GraphQLList(BookingType),
             resolve(parent, args) {
-                return Booking.find({ userId: parent.id });
+                return Booking.find({ userID: parent.id });
             }
         }
     })
@@ -43,9 +37,7 @@ const UserType = new GraphQLObjectType({
 
 const BookingType = new GraphQLObjectType({
     name: 'Booking',
-    //We are wrapping fields in the function as we dont want to execute this ultil 
-    //everything is inilized. For example below code will throw error AuthorType not 
-    //found if not wrapped in a function
+
     fields: () => ({
         id: { type: GraphQLID },
         user: {
@@ -63,21 +55,15 @@ const BookingType = new GraphQLObjectType({
     })
 });
 
-//RootQuery describe how users can use the graph and grab data.
-//E.g Root query to get all authors, get all books, get a particular 
-//book or get a particular author.
+
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
         user: {
             type: UserType,
-            //argument passed by the user while making the query
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-                //Here we define how to get data from database source
 
-                //this will return the book with id passed in argument 
-                //by the user
                 return User.findById(args.id);
             }
         },
@@ -113,17 +99,16 @@ const RootQuery = new GraphQLObjectType({
                 return Booking.find({});
             }
         }
+
     }
 });
 
-//Very similar to RootQuery helps user to add/update to the database.
 const Mutation = new GraphQLObjectType({
     name: 'Mutation',
     fields: {
         addCar: {
             type: CarType,
             args: {
-                //GraphQLNonNull make these field required
                 model: { type: new GraphQLNonNull(GraphQLString) },
                 brand: { type: new GraphQLNonNull(GraphQLString) },
                 price: { type: new GraphQLNonNull(GraphQLInt) }
@@ -142,13 +127,11 @@ const Mutation = new GraphQLObjectType({
             args: {
                 email: { type: new GraphQLNonNull(GraphQLString) },
                 password: { type: new GraphQLNonNull(GraphQLString) },
-                //  authorID: { type: new GraphQLNonNull(GraphQLID) }
             },
             resolve(parent, args) {
                 let user = new User({
                     email: args.email,
                     password: args.password,
-                    //  authorID: args.authorID
                 })
                 return user.save()
             }
@@ -156,7 +139,6 @@ const Mutation = new GraphQLObjectType({
         addBooking: {
             type: BookingType,
             args: {
-                //GraphQLNonNull make these field required
                 userID: { type: new GraphQLNonNull(GraphQLID) },
                 carID: { type: new GraphQLNonNull(GraphQLID) }
             },
@@ -171,8 +153,7 @@ const Mutation = new GraphQLObjectType({
     }
 });
 
-//Creating a new GraphQL Schema, with options query which defines query 
-//we will allow users to use when they are making request.
+
 module.exports = new GraphQLSchema({
     query: RootQuery,
     mutation: Mutation
