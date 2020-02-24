607a0cddf1d807f52ecc6aa9449786be97b39905
Alice <alice.orlovaa@gmail.com>
2020-02-24 19:57:31 +0200

    stavly takim labam klass

---
+++ dzhava 2/lab2/video-game-client/src/test/java/com/example/videogameclient/VideoGameClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.videogameclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoGameClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
